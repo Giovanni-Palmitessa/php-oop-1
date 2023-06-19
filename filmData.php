aff936f5e8f454ef31924e38399e47d0a716191a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-19 15:55:40 +0200

    Add second Object

--- filmData.php
+++ filmData.php
@@ -11,4 +11,14 @@ $movies = [
             new Genres('Avventura', "Genere cinematografico destinato al divertimento e all'evasione, basato sul racconto di imprese rischiose e piene di imprevisti, inserite all'interno di viaggi in luoghi misteriosi e irti di pericoli.", 3),
         ]
     ),
+    new Movie(
+        'Super Mario Bros. - Il film',
+        'English',
+        2023,
+        [
+            new Genres('Animazione', "Da un punto di vista tecnico il cinema di a. si differenzia da quello fotografico, o 'dal vero', per il fatto che l'effetto del movimento è ottenuto attraverso il proiettore e non la macchina da presa.", 4),
+            new Genres('Commedia', "Un film commedia è un genere di film che pone l'accento sull'umorismo. I film di questo genere solitamente hanno un lieto fine. Il genere non va scambiato col film comico, nel quale lo scopo principale è quello di far ridere il pubblico.", 5),
+            new Genres('Avventura', "Genere cinematografico destinato al divertimento e all'evasione, basato sul racconto di imprese rischiose e piene di imprevisti, inserite all'interno di viaggi in luoghi misteriosi e irti di pericoli.", 3),
+        ]
+    ),
 ];
