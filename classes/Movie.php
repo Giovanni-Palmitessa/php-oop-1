e2b9779ee5216fd779df57fcfdd5fb2904e231e0
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-19 15:36:47 +0200

    Add __construct to movie

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,8 +1,27 @@
 <?php
+// instance variables
 class Movie
 {
     public string $name;
     public string $language;
     public int $year;
     public array $genres;
+
+    // constuctor
+
+    public function __construct(string $name, string $language, int $year, array $genres = [])
+    {
+        $this->name = $name;
+        $this->language = $language;
+        $this->year = $year;
+        // ciclo che controlla che ogni indirizzo sia di tipo Genres
+        foreach ($genres as $genre) {
+            if (!$genre instanceof Genres) {
+                die('Gli indirizzi devono essere di tipo Genres');
+            }
+        };
+        $this->genres = $genres;
+        // incremento il contatore che conta gli oggetti instanziati
+        // self::$counter += 1;
+    }
 }
